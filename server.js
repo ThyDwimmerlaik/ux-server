4bfaf648f156d174c752118c42fb480dc8777821
Daniel Zurita <zurita.daniel@hotmail.com>
2015-09-07 11:49:06 -0500

    prueba de la instancia
    Probando si la instancia de amazon puede correr ambos servidores

---
+++ server.js
@@ -0,0 +1,78 @@
+var http = require('http');
+var mysql = require('mysql');
+var qs = require('querystring');
+var conn = require('../db/connection.json');
+
+
+var pool = mysql.createPool({
+  connectionLimit :   100,
+  host :              conn.host,
+  user :              conn.user,
+  password :          conn.pass,
+  database :          conn.database,
+  debug :             false
+});
+
+function handleDB(q,callback){
+  pool.getConnection(function(err,connection){
+    if(err){
+      connection.release();
+      writeLog(err.message);
+      return;
+    }
+    connection.query(q,function(err,rows,fields){
+      connection.release();
+      if(!err){
+        if(rows.length > 0){
+          var RowsFields = [];
+          for (var i in rows){
+            RowsFields[i] = rows[i];
+          }
+          //console.log('Data query and printed successfully!');
+          callback(RowsFields);
+        }
+        else{
+          //writeLog('Data query and inserted successfully!');
+          return;
+        }
+      }
+      else{
+        writeLog(err.message);
+        return;
+      }
+    });
+    connection.on('error', function(err) {      
+      connection.release();
+      writeLog(err.message);
+      return;
+    });
+  });
+}
+
+http.createServer(function(req,res){
+  switch(req.url){
+    case '/':
+      res.writeHead(200,'OK',{'Content-Type':'text/html'});
+      res.end();
+    break;
+    case '/login':
+
+    break;
+    case '/get_lights':
+        
+    break;
+    case '/post_lights':
+
+    break;
+
+    default:
+      console.log('[404] '+req.method+' to '+req.url);
+      res.writeHead('404','Not found',{'Content-Type':'text/html'});
+      res.end('<html><head><title>ERROR</title></head><body><h1>NOT SUPPORTED</h1></body></html>');
+  }
+}).listen(4949,function(err){
+  if(!err)
+    writeLog('[INFO] Listening on 8080');
+  else
+    writeLog('[INFO] '+err.message);
+  });
